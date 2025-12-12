e7a9215203e6e6cb3ecf0a9028d189a9b0efe5d1
Samir <samir_kerimov_01@mail.ru>
2025-12-13 02:16:40 +0500

    feat: add Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "../styles/layouts/Home.module.scss";
+import { bg_img } from "../utils/reExportImg";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className={styles.wrapper}>
+      <Navbar />
+      <div className={styles.home__block}>
+        <img src={bg_img} alt="" className={styles.home__block_img} />
+        <div className={styles.home__block_text}>
+          <span>Привет ! Я</span>
+          <h1>Самир</h1>
+          <span>И я Frontend-разработчик</span>
+        </div>
+      </div>
+    </div>
+  );
 }
