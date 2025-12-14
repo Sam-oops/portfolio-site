bee1d023096db7c21c7275b85f8d2f84ea44e5d7
Samir <samir_kerimov_01@mail.ru>
2025-12-14 13:19:47 +0500

    chore: fix context

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,10 @@ import "./styles/layouts/main.scss";
 import "./styles/layouts/Home.module.scss";
 import React from "react";
 import { router } from "./routes";
+import ThemeContextProvider from "./context/themeContext";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <ThemeContextProvider>
+    <RouterProvider router={router} />
+  </ThemeContextProvider>
 );
